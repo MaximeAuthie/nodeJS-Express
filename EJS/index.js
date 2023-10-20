bbab35c9c8842cf61e8642ca8e8876663587e222
Maxime Authié <authie.maxime@orange.fr>
2023-10-20 17:53:14 +0200

    Varaibles, conditions et liste EJS

--- EJS/index.js
+++ EJS/index.js
@@ -14,10 +14,39 @@ app.use('/static', express.static('public'));
 const hostname = '127.0.0.1';
 const port = 3000;
 
+//? Déclarer un tableau d'articles
+
+const articles = [
+    {
+        title       : 'Débuter avec node.js', 
+        category    : 'Développement web'
+    },
+    {
+        title       : 'Node.js vs PHP', 
+        category    : 'Développement web'
+    },
+    {
+        title       : 'Les sockets en C', 
+        category    : 'Programmation'
+    },
+    {
+        title       : 'Quel système d\'exploitation utiliser', 
+        category    : 'Informatique'
+    },
+]
+
 //? Router
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
+
+app.get('/hello/:name', (req, res)  => {
+    res.render('hello', { name: req.params.name });
+});
+
+app.get('/posts', (req, res) => {
+    res.render('posts-list', { posts: articles});
+});
 
 //? Ecouter le port du serveur au  démarrrage du serveur
 app.listen(port, (() => {
